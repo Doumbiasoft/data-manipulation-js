c6b82ea8b8a08b2ec4feb2ee439c9e33337afb9d
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-07-31 10:55:10 -0400

    implemented the part 2

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const dontDoThis =
 
 //------------------------- PART 1 --------------------------------
 console.log(
-  `------------------------- PART 1 --------------------------------`
+  `\n------------------------- PART 1 --------------------------------`
 );
 let yesOrNo;
 
@@ -63,46 +63,45 @@ const moduloValue = (nb) => nb % 5;
 const isDivisibleBy5 =
   moduloValue(n1) + moduloValue(n2) + moduloValue(n3) + moduloValue(n4) === 0;
 yesOrNo = isDivisibleBy5 ? "Yes" : "No";
-console.log(`Are all numbers divisible by 5?  ${yesOrNo}`);
+console.log(`\nAre all numbers divisible by 5?  ${yesOrNo}`);
 
 // Check if the first number is larger than the last. Cache the result in a variable.
 
 const isFirstNumberLargerThanLastNumber = n1 > n4;
 yesOrNo = isFirstNumberLargerThanLastNumber ? "Yes" : "No";
-console.log(`Is the first number ${n1} larger than the last ${n4}? ${yesOrNo}`);
+console.log(
+  `\nIs the first number ${n1} larger than the last ${n4}? ${yesOrNo}`
+);
 
 // Accomplish the following arithmetic chain:
 // * Subtract the first number from the second number.
 
 const subtractResult = n2 - n1;
 console.log(
-  `Subtraction of the first number ${n1} from the second number ${n2} is: ${subtractResult}`
+  `\nSubtraction of the first number ${n1} from the second number ${n2} is: ${subtractResult}`
 );
 
 // * Multiply the result by the third number.
 
 const multiplyResult = subtractResult * n3;
 console.log(
-  `Multiply the result ${subtractResult} by the third number ${n3} is: ${multiplyResult}`
+  `\nMultiply the result ${subtractResult} by the third number ${n3} is: ${multiplyResult}`
 );
 
 // * Find the remainder of dividing the result by the fourth number.
 
 const remainder = multiplyResult % n4;
-console.log(`the remainder of ${multiplyResult}/${n4} is: ${remainder}`);
+console.log(`\nthe remainder of ${multiplyResult}/${n4} is: ${remainder}`);
 
 // Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 
 const isLessThan25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 yesOrNo = isLessThan25 ? "Yes" : "No";
-console.log(`Are all numbers less than 25? ${yesOrNo}`);
+console.log(`\nAre all numbers less than 25? ${yesOrNo}`);
 
-console.log(
-  `-------------------------END PART 1 --------------------------------`
-);
 //------------------------- PART2 ----------------------------------
 console.log(
-  `------------------------- PART 2 --------------------------------`
+  `\n------------------------- PART 2 --------------------------------`
 );
 
 /**
@@ -132,18 +131,93 @@ The average cost of fuel is $3 per gallon.
 
 const totalTripDistance = 1500; //miles
 const budget = 175; //$
-const fuelPerGallonPrice = 3; //$
+const fuelPerGallonPrice = 3; //$ fuel price per gallon
+
+// Cases:
+
+// Cases 1
+const milesPerGallon_55 = 30; // miles per gallon
+const speedPerHour_55 = 55; // miles per hour
+
+// Cases 2
+const milesPerGallon_60 = 28; // miles per gallon
+const speedPerHour_60 = 60; // miles per hour
+
+// Cases 3
+const milesPerGallon_75 = 23; // miles per gallon
+const speedPerHour_75 = 75; // miles per hour
+
+const totalMilesPerGallon =
+  milesPerGallon_55 + milesPerGallon_60 + milesPerGallon_75; //total miles per gallon
+
+const totalSpeedPerHour = speedPerHour_55 + speedPerHour_60 + speedPerHour_75;
 
 // How many gallons of fuel will you need for the entire trip?
+const gallonsNeeded = totalTripDistance / totalMilesPerGallon;
+console.log(
+  `\nGallons of fuel needed for the entire trip are: ${gallonsNeeded.toFixed(
+    2
+  )}`
+);
 
 // Will your budget be enough to cover the fuel expense?
+const fuelExpense = gallonsNeeded * fuelPerGallonPrice;
+const isBudgetEnough = fuelExpense <= budget ? "Yes" : "No";
+
+console.log(
+  `\nWill your budget be enough to cover the fuel expense?: ${isBudgetEnough}`
+);
 
 // How long will the trip take, in hours?
+const tripDuration = totalTripDistance / totalSpeedPerHour;
+console.log(
+  `\nHow long will the trip take, in hours?: ${tripDuration.toFixed(2)} Hour(s)`
+);
 
 // Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
 
+// Cases 1 : 55
+const gallonsNeeded_55 = totalTripDistance / milesPerGallon_55;
+const fuelExpense_55 = gallonsNeeded_55 * fuelPerGallonPrice;
+const isBudgetEnough_55 = fuelExpense_55 <= budget ? "Yes" : "No";
+const tripDuration_55 = totalTripDistance / speedPerHour_55;
+
+// Cases 2 : 60
+const gallonsNeeded_60 = totalTripDistance / milesPerGallon_60;
+const fuelExpense_60 = gallonsNeeded_60 * fuelPerGallonPrice;
+const isBudgetEnough_60 = fuelExpense_60 <= budget ? "Yes" : "No";
+const tripDuration_60 = totalTripDistance / speedPerHour_60;
+
+// Cases 3 : 75
+const gallonsNeeded_75 = totalTripDistance / milesPerGallon_75;
+const fuelExpense_75 = gallonsNeeded_75 * fuelPerGallonPrice;
+const isBudgetEnough_75 = fuelExpense_75 <= budget ? "Yes" : "No";
+const tripDuration_75 = totalTripDistance / speedPerHour_75;
+
 // Log the results of your calculations using string concatenation or template literals.
 
+const dataForTable = {};
+
+dataForTable.One_Case_55 = {
+  "Gallons Needed": gallonsNeeded_55.toFixed(2),
+  "Fuel Expense($)": fuelExpense_55.toFixed(2),
+  "Budget Enough": isBudgetEnough_55,
+  "Trip Duration (hours)": tripDuration_55.toFixed(2),
+};
+dataForTable.Two_Case_60 = {
+  "Gallons Needed": gallonsNeeded_60.toFixed(2),
+  "Fuel Expense($)": fuelExpense_60.toFixed(2),
+  "Budget Enough": isBudgetEnough_60,
+  "Trip Duration (hours)": tripDuration_60.toFixed(2),
+};
+dataForTable.Three_Case_75 = {
+  "Gallons Needed": gallonsNeeded_75.toFixed(2),
+  "Fuel Expense($)": fuelExpense_75.toFixed(2),
+  "Budget Enough": isBudgetEnough_75,
+  "Trip Duration (hours)": tripDuration_75.toFixed(2),
+};
 console.log(
-  `-------------------------END PART 2 --------------------------------`
+  `\nCompare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?`
 );
+console.log(`\n--------------------------- TABLE ----------------------------`);
+console.table(dataForTable);
